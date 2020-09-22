aeb0d350e5c47402e5d25085378e2b68b395f5d2
AndreyDuda <andreyduda88@gmail.com>
2020-09-23 02:23:41 +0300

    translation

--- src/Controller/Auth/SignUpController.php
+++ src/Controller/Auth/SignUpController.php
@@ -9,14 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SignUpController extends AbstractController
 {
     private $logger;
+    private $translator;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, TranslatorInterface $translator)
     {
         $this->logger = $logger;
+        $this->translator = $translator;
     }
 
     /**
@@ -39,7 +42,7 @@ class SignUpController extends AbstractController
                 return $this->redirectToRoute('home');
             } catch (\DomainException $e) {
                 $this->logger->error($e->getMessage(), ['exception' => $e]);
-                $this->addFlash('error', $e->getMessage());
+                $this->addFlash('error', $this->translator->trans($e->getMessage(), [], 'exceptions'));
             }
         }
 
