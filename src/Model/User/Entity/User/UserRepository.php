3bdbc6409637da4d5062009ed9eba33a4808436e
AndreyDuda <andreyduda88@gmail.com>
2020-09-23 02:06:55 +0300

    add AuthController part 1

--- src/Model/User/Entity/User/UserRepository.php
+++ src/Model/User/Entity/User/UserRepository.php
@@ -3,17 +3,102 @@ declare(strict_types=1);
 
 namespace App\Model\User\Entity\User;
 
-Interface UserRepository
+use Doctrine\ORM\EntityManager;
+use App\Model\EntityNotFoundException;
+
+class UserRepository
 {
-    public function findByConfirmToken(string $token): ?User;
+    private $em;
 
-    public function hasByEmail(Email $email): bool;
+    private $repo;
 
-    public function hasByNetworkIdentity(string $network, string $identity): bool;
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(User::class);
+    }
 
-    public function add(User $user): void;
+    /**
+     * @param string $token
+     * @return User|Object|null
+     */
+    public function findByConfirmToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['confirmToken' => $token]);
+    }
 
-    public function getByEmail(Email $email): User;
+    /**
+     * @param string $token
+     * @return User|Object|null
+     */
+    public function findByResetToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['resetToken.token' => $token]);
+    }
 
-    public function get(Id $id): User;
+    /**
+     * @param Id $id
+     * @return User|Object
+     * @throws EntityNotFoundException
+     */
+    public function get(Id $id): User
+    {
+        if (!$user = $this->repo->find($id->getvalue())) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
+
+    /**
+     * @param Email $email
+     * @return User|Object
+     * @throws EntityNotFoundException
+     */
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getvalue()])) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
+
+    /**
+     * @param Email $email
+     * @return bool
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    /**
+     * @param string $network
+     * @param string $identity
+     * @return bool
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function hasByNetworkIdentity(string $network, string $identity): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->innerJoin('t.network', 'n')
+                ->andWhere('n.network = :network and n.identity')
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    /**
+     * @param User $user
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
 }
--- src/Model/User/UseCase/Network/Auth/Command.php
+++ src/Model/User/UseCase/Network/Auth/Command.php
@@ -1,17 +1,23 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Model\User\UseCase\Network;
+namespace App\Model\User\UseCase\Network\Auth;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
 {
     /**
      * @var string
+     * @Assert\NotBlank
+     * @Assert\Email()
      */
     public $network;
 
     /**
      * @var string
+     * @Assert\NotBlank
+     * @Assert\Length(min="6")
      */
     public $identity;
 }
