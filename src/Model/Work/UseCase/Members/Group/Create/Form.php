aad187aacc875da2f36b6e5b9c4bc587e36fc371
AndreyDuda <andreyduda88@gmail.com>
2020-10-11 23:08:28 +0300

    realise logic for groups of work

--- src/Model/Work/UseCase/Members/Group/Create/Form.php
+++ src/Model/Work/UseCase/Members/Group/Create/Form.php
@@ -1,10 +1,26 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Model\Work\UseCase\Members\Group\Create;
 
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class Form
+class Form extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', Type\TextType::class);
+    }
 
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class
+        ));
+    }
 }
--- src/Model/Work/UseCase/Members/Group/Edit/Form.php
+++ src/Model/Work/UseCase/Members/Group/Edit/Form.php
@@ -1,10 +1,27 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Model\Work\UseCase\Members\Group\Edit;
 
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class Form
+class Form extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class);
+    }
 
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class
+        ]);
+    }
 }
