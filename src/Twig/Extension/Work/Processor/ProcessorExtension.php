f3d9e38769d874b97f9480dfc1b7e864ee660bb7
AndreyDuda <andreyduda88@gmail.com>
2020-10-20 23:30:32 +0300

    add markdown

---
+++ src/Twig/Extension/Work/Processor/ProcessorExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension\Work\Processor;
+
+use Twig\Extension\AbstractExtension;
+use App\Twig\Extension\Work\Processor\Driver\Driver;
+use Twig\TwigFilter;
+use Webmozart\Assert\Assert;
+
+class ProcessorExtension extends AbstractExtension
+{
+    /** @var Driver[] */
+    private $drivers;
+
+    public function __construct(iterable $drivers)
+    {
+        $this->drivers = $drivers;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('work_processor', [$this, 'process'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function process(?string $text): string
+    {
+        $result = $text;
+        foreach ($this->drivers as $driver) {
+            $result = $driver->process($result);
+        }
+        return $result;
+    }
+}
